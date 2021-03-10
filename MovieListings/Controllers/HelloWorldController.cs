f1293fea72a0ae98fc33a2cf35c8f3c8a2e69f6a
Guillaume Boulben <red56260@gmail.com>
2021-03-10 16:25:11 +0000

    truc

---
+++ MovieListings/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace MovieListings.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "this is index";
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //return "this is mhy welcome";
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+            //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
---
+++ MovieListings/Data/MvcMovieContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieListings.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieListings.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie {get; set;}
+
+    }
+}
